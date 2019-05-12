4cb9b0fec1c1405d380170b7dc9ddfee0ed28aa6
Raj KB <magepsycho@gmail.com>
2019-05-12 22:00:01 +0400

    init

---
+++ README.md
@@ -0,0 +1,59 @@
+# Bash Script: Backup Wordpress Code + Database
+
+This utility script helps you to backup Wordpress code and database.   
+You can either run the command manually or can automate it via cronjob.
+
+
+## INSTALL
+You can simply download the script file and give the executable permission.
+```
+curl -0 https://raw.githubusercontent.com/MagePsycho/wordpress-db-code-backup-bash-script/master/src/wp-db-code-backup.sh -o wp-backup.sh
+chmod +x wp-backup.sh
+```
+
+To make it system wide command
+```
+sudo mv wp-backup.sh /usr/local/bin/wp-backup
+```
+
+## USAGE
+### To display help
+```
+./wp-backup.sh --help
+```
+
+### To backup database only
+```
+./wp-backup.sh --type=db --src-dir=/path/to/wp/root --dest-dir=/path/to/destination
+```
+
+### To backup code only
+```
+./wp-backup.sh --type=code --skip-uploads --src-dir=/path/to/wp/root --dest-dir=/path/to/destination
+```
+- `--skip-uploads` option excludes `wp-content/uploads` folder
+
+### To backup code + database
+```
+./wp-backup.sh --type=all --skip-uploads --src-dir=/path/to/wp/root --dest-dir=/path/to/destination
+```
+
+*You can omit `--src-dir` option if you are running the script as system-wide command from root folder of wordpress*
+
+### To schedule backup via Cron
+If you want to schedule via Cron, just add the following line in your Crontab entry `crontab -e`
+```
+0 0 * * * /path/to/wp-backup.sh --type=all --skip-uploads --src-dir=/path/to/wp/root --dest-dir=/path/to/destination > /dev/null 2>&1
+```
+`0 0 * * *` expression means the command will run run at every midnight.
+
+## Screenshots
+![WordpressBackup Help](https://github.com/MagePsycho/magento2-db-code-backup-bash-script/raw/master/docs/wordpress-backup-script-help.png "Mage2Backup Help")
+1. Screentshot - WordpressBackup Help
+
+## TO-DOS
+- Rotation for backups
+- Enable remote backups
+    - S3
+    - Google Drive
+    - Dropbox
---
+++ src/wp-db-code-backup.sh
@@ -0,0 +1,457 @@
+#!/bin/bash
+
+#
+# Script to backup Wordpress Codebase + Database
+#
+# @author   Raj KB <magepsycho@gmail.com>
+# @website  https://www.magepsycho.com
+# @version  0.1.0
+
+# UnComment it if bash is lower than 4.x version
+shopt -s extglob
+
+################################################################################
+# CORE FUNCTIONS - Do not edit
+################################################################################
+#
+# VARIABLES
+#
+_bold=$(tput bold)
+_underline=$(tput sgr 0 1)
+_reset=$(tput sgr0)
+
+_purple=$(tput setaf 171)
+_red=$(tput setaf 1)
+_green=$(tput setaf 76)
+_tan=$(tput setaf 3)
+_blue=$(tput setaf 38)
+
+#
+# HEADERS & LOGGING
+#
+function _debug()
+{
+    if [[ "$DEBUG" = 1 ]]; then
+        "$@"
+    fi
+}
+
+function _header()
+{
+    printf '\n%s%s==========  %s  ==========%s\n' "$_bold" "$_purple" "$@" "$_reset"
+}
+
+function _arrow()
+{
+    printf '➜ %s\n' "$@"
+}
+
+function _success()
+{
+    printf '%s✔ %s%s\n' "$_green" "$@" "$_reset"
+}
+
+function _error() {
+    printf '%s✖ %s%s\n' "$_red" "$@" "$_reset"
+}
+
+function _warning()
+{
+    printf '%s➜ %s%s\n' "$_tan" "$@" "$_reset"
+}
+
+function _underline()
+{
+    printf '%s%s%s%s\n' "$_underline" "$_bold" "$@" "$_reset"
+}
+
+function _bold()
+{
+    printf '%s%s%s\n' "$_bold" "$@" "$_reset"
+}
+
+function _note()
+{
+    printf '%s%s%sNote:%s %s%s%s\n' "$_underline" "$_bold" "$_blue" "$_reset" "$_blue" "$@" "$_reset"
+}
+
+function _die()
+{
+    _error "$@"
+    exit 1
+}
+
+function _safeExit()
+{
+    exit 0
+}
+
+#
+# UTILITY HELPER
+#
+function _seekConfirmation()
+{
+  printf '\n%s%s%s' "$_bold" "$@" "$_reset"
+  read -p " (y/n) " -n 1
+  printf '\n'
+}
+
+# Test whether the result of an 'ask' is a confirmation
+function _isConfirmed()
+{
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        return 0
+    fi
+    return 1
+}
+
+
+function _typeExists()
+{
+    if type "$1" >/dev/null; then
+        return 0
+    fi
+    return 1
+}
+
+function _isOs()
+{
+    if [[ "${OSTYPE}" == $1* ]]; then
+      return 0
+    fi
+    return 1
+}
+
+function _checkRootUser()
+{
+    #if [ "$(id -u)" != "0" ]; then
+    if [ "$(whoami)" != 'root' ]; then
+        echo "You have no permission to run $0 as non-root user. Use sudo"
+        exit 1;
+    fi
+
+}
+
+function _printPoweredBy()
+{
+    local mp_ascii
+    mp_ascii='
+   __  ___              ___               __
+  /  |/  /__ ____ ____ / _ \___ __ ______/ /  ___
+ / /|_/ / _ `/ _ `/ -_) ___(_-</ // / __/ _ \/ _ \
+/_/  /_/\_,_/\_, /\__/_/  /___/\_, /\__/_//_/\___/
+            /___/             /___/
+'
+    cat <<EOF
+${_green}
+Powered By:
+$mp_ascii
+
+ >> Store: ${_reset}${_underline}${_blue}https://www.magepsycho.com${_reset}${_reset}${_green}
+ >> Blog:  ${_reset}${_underline}${_blue}https://blog.magepsycho.com${_reset}${_reset}${_green}
+
+################################################################
+${_reset}
+EOF
+}
+
+################################################################################
+# SCRIPT FUNCTIONS
+################################################################################
+function _printUsage()
+{
+    echo -n "$(basename "$0") [OPTION]...
+
+Backup Wordpress Codebase + Database.
+Version $VERSION
+
+    Options:
+        -sd,    --src-dir          Source directory (from where backup file will be created, www-dir)
+        -dd,    --dest-dir         Destination directory (to where the backup file will be moved)
+        -bt,    --type             Backup Type. Default: all
+                                   Options:
+                                       1. db (for database only)
+                                       2. code (for codebase only)
+                                       3. all (for database + codebase)
+        -su,    --skip-uploads     Skip wp-content/uploads folder content from code backup.
+        -h,     --help             Display this help and exit
+        -v,     --version          Output version information and exit
+
+    Examples:
+        $(basename "$0") --type=all --skip-uploads --src-dir=... --dest-dir=...
+
+"
+    _printPoweredBy
+    exit 1
+}
+
+function checkCmdDependencies()
+{
+    local _dependencies=(
+      wget
+      cat
+      basename
+      mkdir
+      cp
+      mv
+      rm
+      chown
+      chmod
+      date
+      find
+      awk
+      gzip
+      gunzip
+    )
+
+    for cmd in "${_dependencies[@]}"
+    do
+        hash "${cmd}" &>/dev/null || _die "'${cmd}' command not found."
+    done;
+}
+
+function processArgs()
+{
+    # Parse Arguments
+    for arg in "$@"
+    do
+        case $arg in
+            -bt=*|--type=*)
+                WP_BACKUP_TYPE="${arg#*=}"
+            ;;
+            -sd=*|--src-dir=*)
+                WP_SRC_DIR="${arg#*=}"
+            ;;
+            -dd=*|--dest-dir=*)
+                WP_DEST_DIR="${arg#*=}"
+            ;;
+            -su|--skip-uploads)
+                WP_SKIP_UPLOADS=1
+            ;;
+            -bn=*|--backup-name=*)
+                WP_BACKUP_NAME="${arg#*=}"
+            ;;
+            --debug)
+                DEBUG=1
+                set -o xtrace
+            ;;
+            -h|--help)
+                _printUsage
+            ;;
+            *)
+                #_printUsage
+            ;;
+        esac
+    done
+
+    validateArgs
+    sanitizeArgs
+}
+
+function initDefaultArgs()
+{
+    WP_SRC_DIR=$(pwd)
+}
+
+function sanitizeArgs()
+{
+    # remove trailing /
+    if [[ ! -z "WP_SRC_DIR" ]]; then
+        WP_SRC_DIR="${WP_SRC_DIR%/}"
+    fi
+
+    if [[ ! -z "WP_DEST_DIR" ]]; then
+        WP_DEST_DIR="${WP_DEST_DIR%/}"
+    fi
+}
+
+
+function validateArgs()
+{
+    ERROR_COUNT=0
+    if [[ -z "$WP_BACKUP_TYPE" ]]; then
+        _error "Backup type parameter missing."
+        ERROR_COUNT=$((ERROR_COUNT + 1))
+    fi
+
+    if [[ ! -z "$WP_BACKUP_TYPE" && "$WP_BACKUP_TYPE" != @(db|code|all) ]]; then
+        _error "Backup type must be one of db|code|all."
+        ERROR_COUNT=$((ERROR_COUNT + 1))
+    fi
+
+    if [[ -z "$WP_SRC_DIR" ]]; then
+        _error "Source directory parameter missing."
+        ERROR_COUNT=$((ERROR_COUNT + 1))
+    fi
+
+    if [[ ! -z "$WP_SRC_DIR" && ! -f "$WP_SRC_DIR/wp-config.php" ]]; then
+        _error "Source directory must be Wordpress root folder."
+        ERROR_COUNT=$((ERROR_COUNT + 1))
+    fi
+
+    if [[ -z "$WP_DEST_DIR" ]]; then
+        _error "Destination directory parameter missing."
+        ERROR_COUNT=$((ERROR_COUNT + 1))
+    fi
+
+    if [[ ! -z "$WP_DEST_DIR" ]] && ! mkdir -p "$WP_DEST_DIR"; then
+        _error "Unable to create destination directory."
+        ERROR_COUNT=$((ERROR_COUNT + 1))
+    fi
+
+    #echo "$ERROR_COUNT"
+    [[ "$ERROR_COUNT" -gt 0 ]] && exit 1
+}
+
+function sanitizeArgs()
+{
+    # remove trailing /
+    if [[ ! -z "$WP_SRC_DIR" ]]; then
+        WP_SRC_DIR="${WP_SRC_DIR%/}"
+    fi
+
+    if [[ ! -z "$WP_DEST_DIR" ]]; then
+        WP_DEST_DIR="${WP_DEST_DIR%/}"
+    fi
+}
+
+function prepareBackupName()
+{
+    if [[ -z "$WP_BACKUP_NAME" ]]; then
+        #MD5=`echo \`date\` $RANDOM | md5sum | cut -d ' ' -f 1`
+        DATETIME=$(date +"%Y-%m-%d-%H-%M-%S")
+        WP_BACKUP_NAME="wp-backup.$DATETIME"
+    fi
+}
+
+function prepareCodebaseFilename()
+{
+    WP_CODE_BACKUP_FILE="${WP_DEST_DIR}/${WP_BACKUP_NAME}.tar.gz"
+}
+
+function prepareDatabaseFilename()
+{
+    WP_DB_BACKUP_FILE="${WP_DEST_DIR}/${WP_BACKUP_NAME}.sql.gz"
+}
+
+function createDbBackup()
+{
+    _success "Dumping MySQL..."
+    local host username password dbName
+
+    host=$(grep DB_HOST "${WP_SRC_DIR}/wp-config.php" |cut -d "'" -f 4)
+    username=$(grep DB_USER "${WP_SRC_DIR}/wp-config.php" | cut -d "'" -f 4)
+    password=$(grep DB_PASSWORD "${WP_SRC_DIR}/wp-config.php" | cut -d "'" -f 4)
+    dbName=$(grep DB_NAME "${WP_SRC_DIR}/wp-config.php" |cut -d "'" -f 4)
+
+    # @todo option to skip log tables
+    # @tod FIX Warning: Using a password on the command line interface can be insecure.
+	mysqldump -h "$host" -u "$username" -p"$password" "$dbName" | gzip > "$WP_DB_BACKUP_FILE"
+	_success "Done!"
+}
+
+function createCodeBackup()
+{
+    _success "Archiving Codebase..."
+    declare -a EXC_PATH
+    EXC_PATH[1]=./.git
+    EXC_PATH[2]=./wp-content/cache
+    EXC_PATH[3]=./wp-content/upgrade
+    EXC_PATH[4]=./wp-config.php
+
+    if [[ "$WP_SKIP_UPLOADS" == 1 ]]; then
+        EXC_PATH[5]=./wp-content/uploads
+    fi
+
+    EXCLUDES=''
+    for i in "${!EXC_PATH[@]}" ; do
+        CURRENT_EXC_PATH=${EXC_PATH[$i]}
+        # note the trailing space
+        EXCLUDES="${EXCLUDES}--exclude=${CURRENT_EXC_PATH} "
+    done
+
+    tar -zcf "$WP_CODE_BACKUP_FILE" ${EXCLUDES} -C "${WP_SRC_DIR}" .
+
+	_success "Done!"
+}
+
+function rotateBackups()
+{
+    # @todo
+    # delete all but 5 recent wordpress database back-ups (files having .sql.gz extension) in backup folder.
+    find "${WP_DEST_DIR}" -maxdepth 1 -name "*.sql.gz" -type f | xargs -x ls -t | awk 'NR>5' | xargs -L1 rm
+
+    # delete all but 5 recent wordpress files back-ups (files having .tar.gz extension) in backup folder.
+    find "${WP_DEST_DIR}" -maxdepth 1 -name "*.tar.gz" -type f | xargs -x ls -t | awk 'NR>5' | xargs -L1 rm
+}
+
+function printSuccessMessage()
+{
+    _success "Wordpress Backup Completed!"
+
+    echo "################################################################"
+    echo ""
+    echo " >> Backup Type           : ${WP_BACKUP_TYPE}"
+    echo " >> Backup Source         : ${WP_SRC_DIR}"
+    if [[ $WP_BACKUP_TYPE = @(db|database|all) ]]; then
+        echo " >> Database Dump File    : ${WP_DB_BACKUP_FILE}"
+    fi
+
+    if [[ $WP_BACKUP_TYPE = @(codebase|code|all) ]]; then
+        echo " >> Codebase Archive File : ${WP_CODE_BACKUP_FILE}"
+    fi
+
+    echo ""
+    echo "################################################################"
+    _printPoweredBy
+
+}
+
+################################################################################
+# Main
+################################################################################
+export LC_CTYPE=C
+export LANG=C
+
+DEBUG=0
+_debug set -x
+VERSION="0.1.0"
+
+WP_SRC_DIR=
+WP_DEST_DIR=
+WP_BACKUP_TYPE=all
+WP_SKIP_UPLOADS=0
+WP_BACKUP_NAME=
+WP_DB_BACKUP_FILE=
+WP_CODE_BACKUP_FILE=
+
+function main()
+{
+    #_checkRootUser
+    checkCmdDependencies
+
+    [[ $# -lt 1 ]] && _printUsage
+
+    initDefaultArgs
+    processArgs "$@"
+
+    prepareBackupName
+    prepareCodebaseFilename
+    prepareDatabaseFilename
+
+    if [[ "$WP_BACKUP_TYPE" = @(database|db|all) ]]; then
+        createDbBackup
+    fi
+
+    if [[ "$WP_BACKUP_TYPE" = @(codebase|code|all) ]]; then
+        createCodeBackup
+    fi
+
+    printSuccessMessage
+
+    exit 0
+}
+
+main "$@"
+
+_debug set +x
